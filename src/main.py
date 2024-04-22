fee29b67ea96f03ae5a2f4de0bef7c874dd73fd8
Dmytro-Tarasenko <ug.dotenko@gmail.com>
2024-04-22 14:36:50 +0300

    Finish

--- src/main.py
+++ src/main.py
@@ -1,11 +1,29 @@
+import re
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from contacts.routes import router
 
 app = FastAPI()
 
 app.include_router(router)
+
+@app.middleware("http")
+async def define_response(request: Request,
+                          call_next):
+    browser_regexp = (r"Mozilla|Chrome|Chromium|Apple|WebKit|" +
+                      r"Edge|IE|MSIE|Firefox|Gecko")
+    ua_string = request.headers.get('user-agent')
+    if re.search(browser_regexp, ua_string):
+        response_type = "html"
+    else:
+        response_type = "api"
+    request.headers.__dict__["_list"].append(
+        ("response-type".encode(), response_type.encode())
+    )
+    response = await call_next(request)
+    return response
 
 
 if __name__ == "__main__":
